8ccf67e95a4d3f9b650a438db86285a8bc03f750
Mirlan Duishenbaev <duyshenbaev@gmail.com>
2022-04-11 17:04:06 +0600

    app has been refactored.

--- week_3/Managed_Threading_Task2/Managed_Threading_Task2/Program.cs
+++ week_3/Managed_Threading_Task2/Managed_Threading_Task2/Program.cs
@@ -1,27 +1,34 @@
 ﻿using Managed_Threading_Task2;
-
-
-Thread t1, t2, t3;
-string url = "https://jsonplaceholder.typicode.com/photos";
-var list = Image.CreateImageList(url);
-
-Random random1 = new Random();
-Random random2 = new Random();
-Random random3 = new Random();
-
-for (int i = 0; i < 10; i++)
+public class Programm
 {
-    int index1 = random1.Next(list.Count);
-    int index2 = random1.Next(list.Count);
-    int index3 = random1.Next(list.Count);
-    t1 = new Thread(() => Image.DownloadImage(list[index1].ThumbnailUrl, list[index1].Id));
-    t1.Start();
-    t2 = new Thread(() => Image.DownloadImage(list[index2].ThumbnailUrl, list[index2].Id));
-    t2.Start();    
-    t3 = new Thread(() => Image.DownloadImage(list[index3].ThumbnailUrl, list[index3].Id));
-    t3.Start();
-    Thread.Sleep(3000);
+    static void Main(string[] args)
+    {
+        Thread t1, t2, t3;
+        string url = "https://jsonplaceholder.typicode.com/photos";
+        var list = Image.CreateImageList(url);
+
+        Random random1 = new Random();
+        Random random2 = new Random();
+        Random random3 = new Random();
+
+        for (int i = 0; i < 10; i++)
+        {
+            int index1 = random1.Next(list.Count);
+            int index2 = random1.Next(list.Count);
+            int index3 = random1.Next(list.Count);
+            t1 = new Thread(() => Image.DownloadImage(list[index1].ThumbnailUrl, list[index1].Id));
+            t1.Start();
+            t2 = new Thread(() => Image.DownloadImage(list[index2].ThumbnailUrl, list[index2].Id));
+            t2.Start();
+            t3 = new Thread(() => Image.DownloadImage(list[index3].ThumbnailUrl, list[index3].Id));
+            t3.Start();
+            Console.WriteLine("----------------------------------------------------");
+            Thread.Sleep(3000);
+        }
+    }
 }
+
+
 
 
 
