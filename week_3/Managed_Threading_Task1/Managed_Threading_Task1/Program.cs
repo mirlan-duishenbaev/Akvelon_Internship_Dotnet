0789441d6131460005e8e84ed1dddf119553d0fc
Mirlan Duishenbaev <duyshenbaev@gmail.com>
2022-04-11 17:05:54 +0600

    app has been refactored.

--- week_3/Managed_Threading_Task1/Managed_Threading_Task1/Program.cs
+++ week_3/Managed_Threading_Task1/Managed_Threading_Task1/Program.cs
@@ -1,53 +1,61 @@
 ﻿using Managed_Threading_Task1;
 
-Thread dancer1, dancer2, dancer3;
-int dance = new();
-int count = 0;
-
-Console.WriteLine("\nDJ starts the music on!");
-
-Thread music = new Thread(() =>
+public class Programm
 {
-    Random rnd = new Random();
-    for (int i = 0; i < 20; i++)
+    static void Main(string[] args)
     {
-        var index = rnd.Next(1, 4);
-        Console.WriteLine("\nTrack #{0}", i+1);
-        Console.WriteLine($"{(Managed_Threading_Task1.enums.Music)index} is playing!");
-        count = i;
-        dance = index;
-        Thread.Sleep(5000);
+        Thread dancer1, dancer2, dancer3;
+        int dance = new();
+        int count = 0;
+
+        Console.WriteLine("\nDJ starts the music on!");
+
+        Thread music = new Thread(() =>
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                var index = rnd.Next(1, 4);
+                Console.WriteLine("\nTrack #{0}", i + 1);
+                Console.WriteLine($"{(Managed_Threading_Task1.enums.Music)index} is playing!");
+                count = i;
+                dance = index;
+                Thread.Sleep(5000);
+            }
+        });
+
+        music.Start();
+
+        while (music.IsAlive)
+        {
+            Thread.Sleep(1000);
+            dancer1 = new Thread(() => DanceFloor.AllAreDancing(dance))
+            {
+                Name = "John"
+            };
+            dancer2 = new Thread(() => DanceFloor.AllAreDancing(dance))
+            {
+                Name = "Anna"
+            };
+            dancer3 = new Thread(() => DanceFloor.AllAreDancing(dance))
+            {
+                Name = "Brad"
+            };
+            dancer1.Start();
+            dancer2.Start();
+            dancer3.Start();
+            Thread.Sleep(4000);
+        }
+
+        if (count == 19)
+        {
+            music.Join();
+            Console.WriteLine("\nThe disco is over!");
+        }
     }
-});
-
-music.Start();
-
-while (music.IsAlive)
-{
-    Thread.Sleep(1000);
-    dancer1 = new Thread(() => DanceFloor.AllAreDancing(dance))
-    {
-        Name = "John"
-    };
-    dancer2 = new Thread(() => DanceFloor.AllAreDancing(dance))
-    {
-        Name = "Anna"
-    };
-    dancer3 = new Thread(() => DanceFloor.AllAreDancing(dance))
-    {
-        Name = "Brad"
-    };
-    dancer1.Start();
-    dancer2.Start();
-    dancer3.Start();
-    Thread.Sleep(4000);
 }
 
-if(count == 19)
-{
-    music.Join();
-    Console.WriteLine("\nThe disco is over!");
-}
+
 
 
 
