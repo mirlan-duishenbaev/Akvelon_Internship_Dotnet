9e4e79d334b79d2148baf22df813b91100ea05b7
Mirlan Duishenbaev <duyshenbaev@gmail.com>
2022-04-27 16:58:18 +0600

    implemented reveresing of single-linkedlist

--- week_0/LinkedList_Reorder/LinkedList_Reorder/Program.cs
+++ week_0/LinkedList_Reorder/LinkedList_Reorder/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LinkedList_Reorder;
+
+ListNode listNode = new ListNode();
+Solution.AddNode(listNode, new ListNode(85));
+Solution.AddNode(listNode, new ListNode(12));
+Solution.AddNode(listNode, new ListNode(27));
+Solution.AddNode(listNode, new ListNode(9));
+Solution.AddNode(listNode, new ListNode(15));
+
+ListNode newListNode = Solution.ReverseList(listNode);
+
+Solution.PrintList(listNode);
+Solution.PrintList(newListNode);
+
+Console.ReadKey();
---
+++ week_0/LinkedList_Reorder/LinkedList_Reorder/Solution.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList_Reorder
+{
+    internal class Solution
+    {
+
+        public static ListNode ReverseList(ListNode head)
+        {
+            ListNode current = head, next = null;
+            while (current != null)
+            {
+                ListNode tmp = current.next;
+                current.next = next;
+                next = current;
+                current = tmp;
+            }
+            head = next;
+            return head;
+        }
+
+        public static void AddNode(ListNode head, ListNode node)
+        {
+            if (head == null)
+                head = node;
+            else
+            {
+                ListNode temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+        }
+
+        public static void PrintList(ListNode head)
+        {
+            ListNode current = head;
+            while (current != null)
+            {
+                Console.Write(current.val + " ");
+                current = current.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
